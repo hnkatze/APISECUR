845a009670d31927e8966bb78cc212aeed41aec0
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-10-22 20:13:59 -0600

    Change a some queries for was use name o database of localhost

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,7 +1,7 @@
 export const queries = {
   getAllCompany: "SELECT * FROM Company",
   createNewCompany:
-    "INSERT INTO [APISECUR].[dbo].[Company] ( Id, Name, Description, Code) Values ( @Id, @Name, @Description, @Code)",
+    "INSERT INTO Company ( Id, Name, Description, Code) Values ( @Id, @Name, @Description, @Code)",
   getCompanyById: "SELECT * FROM Company Where Id = @Id",
   deleteCompany: "DELETE FROM Company Where Id = @Id",
   updateCompany:
@@ -10,9 +10,9 @@ export const queries = {
     "SELECT * FROM Employee WHERE CompanyId = @CompanyId",
     getAllEmployee: "SELECT * FROM Employee",
   createNewEmployee:
-    "INSERT INTO [APISECUR].[dbo].[Employee] (Id, Name, LastName, Age, Code, Description, DNI, Placa, CompanyId) VALUES (@Id, @Name, @LastName, @Age, @Code, @Description, @DNI, @Placa, @CompanyId)",
-  getEmployeeById: "SELECT * FROM [APISECUR].[dbo].[Employee] WHERE id = @Id",
+    "INSERT INTO Employee (Id, Name, LastName, Age, Code, Description, DNI, Placa, CompanyId) VALUES (@Id, @Name, @LastName, @Age, @Code, @Description, @DNI, @Placa, @CompanyId)",
+  getEmployeeById: "SELECT * FROM Employee WHERE id = @Id",
   updateEmployee:
-    "UPDATE [APISECUR].[dbo].[Employee] SET Name = @Name, LastName = @LastName, Age = @Age, Code = @Code, Description = @Description, DNI = @DNI, Placa = @Placa WHERE id = @Id",
-  deleteEmployee: "DELETE FROM [APISECUR].[dbo].[Employee] WHERE id = @Id",
+    "UPDATE Employee SET Name = @Name, LastName = @LastName, Age = @Age, Code = @Code, Description = @Description, DNI = @DNI, Placa = @Placa WHERE id = @Id",
+  deleteEmployee: "DELETE FROM Employee WHERE id = @Id",
 };
