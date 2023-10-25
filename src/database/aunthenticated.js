8bdd0dcb82fab1e50f150ebfdcbbe55e418153e0
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-10-24 21:36:50 -0600

    Missing space in the code

--- src/database/aunthenticated.js
+++ src/database/aunthenticated.js
@@ -5,7 +5,7 @@ import config from "../config";
 export function authenticateApiKey(req, res, next) {
   const apiKey = req.headers['api-key'];
 
-  if (!apiKey || apiKey !== config .apiKeyHeaders) {
+  if (!apiKey || apiKey !== config.apiKeyHeaders) {
     return res.sendStatus(401);
   }
 
