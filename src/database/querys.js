e10f9e34b31387c72f17eaa685800461c4486eb3
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-11-11 00:39:26 -0600

    Created a new queries for scan tables

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,18 +1,19 @@
 export const queries = {
   getAllCompany: "SELECT * FROM Company",
-  createNewCompany:
-    "INSERT INTO Company (Id, Name, Description, Code) VALUES (?, ?, ?, ?)",
+  createNewCompany: "INSERT INTO Company (Id, Name, Description, Code) VALUES (?, ?, ?, ?)",
   getCompanyById: "SELECT * FROM Company WHERE Id = ?",
   deleteCompany: "DELETE FROM Company WHERE Id = ?",
-  updateCompany:
-    "UPDATE Company SET Name = ?, Description = ?, Code = ? WHERE Id = ?",
-  getAllEmployeesByCompany:
-    "SELECT * FROM Employee WHERE CompanyId = ?",
+  updateCompany: "UPDATE Company SET Name = ?, Description = ?, Code = ? WHERE Id = ?",
+  getAllEmployeesByCompany: "SELECT * FROM Employee WHERE CompanyId = ?",
   getAllEmployee: "SELECT * FROM Employee",
-  createNewEmployee:
-    "INSERT INTO Employee (Id, CompanyId, Name, LastName, Age, Code, Description, DNI, Placa) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+  createNewEmployee: "INSERT INTO Employee (Id, CompanyId, Name, LastName, Age, Code, Description, DNI, Placa) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
   getEmployeeById: "SELECT * FROM Employee WHERE Id = ?",
-  updateEmployee:
-    "UPDATE Employee SET Name = ?, LastName = ?, Age = ?, Code = ?, Description = ?, DNI = ?, Placa = ? WHERE Id = ?",
+  updateEmployee: "UPDATE Employee SET Name = ?, LastName = ?, Age = ?, Code = ?, Description = ?, DNI = ?, Placa = ? WHERE Id = ?",
   deleteEmployee: "DELETE FROM Employee WHERE Id = ?",
+  getAllScan: "SELECT * FROM Scan",
+  createNewScan: "INSERT INTO Scan (Id, ScanTime, Location, EmployeeId, CompanyId) VALUES (?, ?, ?, ?, ?)",
+  getScanById: "SELECT * FROM Scan WHERE Id = ?",
+  deleteScan: "DELETE FROM Scan WHERE Id = ?",
+  updateScan: "UPDATE Scan SET ScanTime = ?, Location = ?, EmployeeId = ?, CompanyId = ? WHERE Id = ?",
 };
+
